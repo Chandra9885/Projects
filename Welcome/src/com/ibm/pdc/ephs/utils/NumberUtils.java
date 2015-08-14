866d7d7c35977b1ecfd8e7184c2574f4c8defbe2
Chandra988 <chandrahasareddy369@gmail.com>
2015-08-14 19:57:26 +0530

    NumberUtils class creation

---
+++ Welcome/src/com/ibm/pdc/ephs/utils/NumberUtils.java
@@ -0,0 +1,32 @@
+/**
+ *******************************************************************************
+ * Licensed Materials - Property of IBM                                        *
+ * © Copyright IBM 2007 All Rights Reserved                                 *
+ * Created on 14-Aug-2015                                                          *
+ *******************************************************************************
+ * Please do not makes any changes to this file without first                  *
+ * updating the revision history below, under description                      *
+ * please include relevant CRs or defect ids that required the change          *
+ *******************************************************************************
+ * Revision History:                                                           *
+ *                                                                             *
+ * Date        Name              Reference   Description                       *
+ * ----------  ----------------  --------------------------------------------- *
+ * 14-Aug-2015    Chandra            created                              *
+ *******************************************************************************
+ */
+
+package com.ibm.pdc.ephs.utils;
+
+/**
+ * @author Chandrahasa
+ *
+ */
+public class NumberUtils
+{
+
+	public void compareNumbers(int i, int j)
+	{
+
+	}
+}
